e3042506a27f436bc97210c5f0499c26465b1041
Justas <jmedeisis@gmail.com>
2015-02-23 13:18:06 +0000

    Third session. 23/02/2015. Create DetailActivity and launch it on list item click.

--- app/src/main/java/uk/co/icappsoc/projectsunshine/ForecastFragment.java
+++ app/src/main/java/uk/co/icappsoc/projectsunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package uk.co.icappsoc.projectsunshine;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,7 +15,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -84,7 +84,9 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String forecast = forecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
             }
         });
         listView.setAdapter(forecastAdapter);
