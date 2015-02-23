dc8cf4befeff0d27c2e9d39b4dd748ca5475a1fc
Justas <jmedeisis@gmail.com>
2015-02-23 14:01:19 +0000

    Third session. 23/02/2015. Renamed new fragment to DetailFragment.

--- app/src/main/java/uk/co/icappsoc/projectsunshine/DetailActivity.java
+++ app/src/main/java/uk/co/icappsoc/projectsunshine/DetailActivity.java
@@ -20,7 +20,7 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -49,11 +49,11 @@ public class DetailActivity extends ActionBarActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * A fragment displaying weather details.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
 
-        public PlaceholderFragment() { }
+        public DetailFragment() { }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
